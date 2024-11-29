43691917573381e33c1dc5376082a801c694c59d
Masha Ushakova <masha.a.ushakova@gmail.com>
2024-11-29 09:37:17 +0100

    Logger added

--- mqtt.py
+++ mqtt.py
@@ -5,6 +5,9 @@ import time
 import threading
 import RPi.GPIO as GPIO
 
+from loguru import logger
+import sys
+
 # Set the GPIO mode (BCM or BOARD)
 GPIO.setmode(GPIO.BCM)
 
@@ -22,58 +25,78 @@ class MQTTServer:
         self._broker = "localhost"
         self._port = 1883
         self._topic = "weird-stuff"
+        
         # set up MQTT client
         self._client = mqtt.Client()
         self._client.username_pw_set("pico", "ndl@group3")
         self._client.on_message = self._on_message
         self._connect()
+        
         # subscribe to the topic I need to listen to
         self._client.subscribe("magnetic_lock")
-
+        
+        # configure log format
+        logger.remove(0)
+        logger.add(sys.stderr, serialize=True)
+      
+        
     @classmethod
     def _on_message(cls, client, userdata, msg):
         match msg.topic:
             case "":
                 payload = msg.payload.decode()
-                print(payload)
             case "magnetic_lock":
                 payload = msg.payload.decode()
-                print(payload)
                 if payload == "close":
-                    print("close")
                     GPIO.output(RELAY_PIN, GPIO.LOW)
+                    
+                    # logging
+                    if not GPIO.input(RELAY_PIN):
+                        logger.success("The lock closed successfully.")
+                    else:
+                        logger.error("The lock close failed.")
+                    client.publish(str(logger), "logs")
+                    
+                    # wait
                     time.sleep(2)
                 else:
-                    print("open")
                     GPIO.output(RELAY_PIN, GPIO.HIGH)
+                    
+                    # logging
+                    if GPIO.input(RELAY_PIN):
+                        logger.success("The lock opened successfully.")
+                    else:
+                        logger.error("The lock open failed.")
+                    client.publish(str(logger), "logs")
+                    
+                    # wait
                     time.sleep(2)
             case _:
                 pass
 
+
     def _connect(self):
         self._client.connect(self._broker, self._port)
 
+
     def _mqtt_loop(self):
         self._client.loop_start()
-
+        
     def send_message(self, message, topic=None):
         if not topic:
             topic = self._topic
         self._client.publish(topic, message)
-
+        
     # subscriber; topic: logs
     def logs_dump(self):
         pass
 
-    # subscriber; topic: lock_status
-    def lock_status(self):
-        pass
 
     def _run(self):
         self._mqtt_loop()
         try:
             while True:
-                # self.send_message("hello")
+                # self.send_message(str(logger), "logs")
                 time.sleep(1)
         except KeyboardInterrupt:
             print("Publisher stopped")
@@ -84,9 +107,11 @@ class MQTTServer:
         server_thread = threading.Thread(target=self._run)
         server_thread.start()
 
+
 def main():
     mqtt = MQTTServer()
     mqtt.run()
+    
     
 if __name__ == "__main__":
     main()
